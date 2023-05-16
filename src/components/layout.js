f4e35cf0f743591c2967e9d606b785dbfc7870d6
Sahil Sangwan <sah.sangwan@gmail.com>
2023-05-16 11:04:23 -0400

    add styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,18 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import parse from "html-react-parser"
 
 const Layout = ({ isHomePage, children }) => {
-  const {
-    wp: {
-      generalSettings: { title },
-    },
-  } = useStaticQuery(graphql`
+    const {
+        wp: {
+            generalSettings: { title },
+        },
+        allWpMenu: {
+            nodes: {
+                0: { // why 0 here?
+                    menuItems: { nodes }
+                }
+            }
+        }
+    } = useStaticQuery(graphql`
     query LayoutQuery {
       wp {
         generalSettings {
@@ -15,34 +22,55 @@ const Layout = ({ isHomePage, children }) => {
           description
         }
       }
+      allWpMenu {
+        nodes {
+          menuItems {
+            nodes {
+              label
+              url
+            }
+          }
+        }
+      }
     }
   `)
 
-  return (
-    <div className="global-wrapper" data-is-root-path={isHomePage}>
-      <header className="global-header">
-        {isHomePage ? (
-          <h1 className="main-heading">
-            <Link to="/">{parse(title)}</Link>
-          </h1>
-        ) : (
-          <Link className="header-link-home" to="/">
-            {title}
-          </Link>
-        )}
-      </header>
+    return (
+        <div className="global-wrapper" data-is-root-path={isHomePage}>
+            <header className="global-header">
+                {isHomePage ? (
+                    <h1 className="main-heading">
+                        <Link to="/">{parse(title)}</Link>
+                    </h1>
+                ) : (
+                    <Link className="header-link-home" to="/">
+                        {title}
+                    </Link>
+                )}
+            </header>
+            <ul>
+                {nodes.map((item, index) => {
+                    const { label, url } = item;
 
-      <main>{children}</main>
+                    return (
+                        <li key={index}>
+                            <Link to={url}>{label}</Link>
+                        </li>
+                    );
+                })}
+            </ul>
 
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-        {` `}
-        And <a href="https://wordpress.org/">WordPress</a>
-      </footer>
-    </div>
-  )
+            <main>{children}</main>
+
+            <footer>
+                © {new Date().getFullYear()}, Built with
+                {` `}
+                <a href="https://www.gatsbyjs.com">Gatsby</a>
+                {` `}
+                And <a href="https://wordpress.org/">WordPress</a>
+            </footer>
+        </div>
+    )
 }
 
 export default Layout
